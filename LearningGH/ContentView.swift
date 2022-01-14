cf178387cf5cbadb0bca25a14fafd24353e1e9ba
Manuel Perdomo <yamuni88@gmail.com>
2022-01-13 20:50:04 -0600

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Manuel Perdomo Yamuni on 13/01/22.
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Manuel Perdomo Yamuni on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
